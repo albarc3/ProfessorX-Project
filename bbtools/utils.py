4d4788e8975cf8886726d72c30da475dfde72163
hpmiguel <miguel.hidalgo@sngular.com>
2022-05-16 17:40:37 +0200

    added time preprocessing jaw nb

--- bbtools/utils.py
+++ bbtools/utils.py
@@ -19,3 +19,22 @@ def filters(raw, fmin = 0.5, fmax = 100., notch = 50., sampling_rate = 250, fir_
     raw_c.filter(l_freq = fmin, h_freq = fmax)
     raw_c.notch_filter(np.arange(notch, sampling_rate/2, notch), fir_design=fir_design)
     return raw_c
+
+from math import floor
+
+def windowfy_dataset(dataframe, chunk_size=100, n_channels=16):
+    n_chunks = floor(dataframe.shape[0] / chunk_size)
+
+    windows = np.ndarray(shape=(n_chunks, chunk_size, n_channels), dtype=float)
+
+    channels_array = []
+    for i in range(1, n_channels + 1):
+        channels_array.append("EEG-ch" + str(i))
+
+    dataframe_as_pd = dataframe[channels_array].to_numpy()
+
+    for index in range(n_chunks):
+        for j in range(chunk_size):
+            windows[index][j] = dataframe_as_pd[index + j]
+
+    return windows
