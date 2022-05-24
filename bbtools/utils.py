c4ba026fc80e4fe1015ecfd4365295cc7e103590
hpmiguel <miguel.hidalgo@sngular.com>
2022-05-24 22:13:04 +0200

    modify functions and training channel

--- bbtools/utils.py
+++ bbtools/utils.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
+from math import floor
 
-def filters(raw, fmin = 0.5, fmax = 100., notch = 50., sampling_rate = 250, fir_design = 'firwin'):
 
+def filters(raw, fmin=0.5, fmax=100., notch=50., sampling_rate=250, fir_design='firwin'):
     '''Apply high/low-pass and notch filters:
 
         === Args ===
@@ -16,40 +18,55 @@ def filters(raw, fmin = 0.5, fmax = 100., notch = 50., sampling_rate = 250, fir_
         * raw_c - mne Raw object: Processed Raw copy
         '''
     raw_c = raw.copy()
-    raw_c.filter(l_freq = fmin, h_freq = fmax)
-    raw_c.notch_filter(np.arange(notch, sampling_rate/2, notch), fir_design=fir_design)
+    raw_c.filter(l_freq=fmin, h_freq=fmax)
+    raw_c.notch_filter(np.arange(notch, sampling_rate / 2, notch), fir_design=fir_design)
     return raw_c
 
-from math import floor
 
-def windowfy_dataset(dataframe, chunk_size=100, n_channels=16):
+def signal_df_to_np(dataframe, chunk_size=100, n_channels=16):
     n_chunks = floor(dataframe.shape[0] / chunk_size)
 
-    windows = np.ndarray(shape=(n_chunks, chunk_size, n_channels), dtype=float)
+    windows = np.ndarray(shape=(n_channels, n_chunks, chunk_size), dtype=float)
 
     channels_array = []
     for i in range(1, n_channels + 1):
         channels_array.append("EEG-ch" + str(i))
 
-    dataframe_as_pd = dataframe[channels_array].to_numpy()
+    channels_as_pd = dataframe[channels_array].to_numpy()
     # shape --> (samplesChannel, nChannels)
 
-    for iChunk in range(n_chunks):
-        for iSample in range(chunk_size):
-            idf = (iChunk * chunk_size) + iSample
-            windows[iChunk][iSample] = dataframe_as_pd[idf]
+    for iChannel in range(n_channels):
+        for iChunk in range(n_chunks):
+            for iSample in range(chunk_size):
+                idf = (iChunk * chunk_size) + iSample
+                windows[iChannel][iChunk][iSample] = channels_as_pd[idf][iChannel]
 
+    # shape --> (channels, windows, samples)
     return windows
 
-import pandas as pd
 
-def labelingWindows(windows, artifactLabel):
-    windowsMap = {'window': [], 'label': []}
-    for i, window in enumerate(windows):
-        even = (i % 2) == 0
-        label = artifactLabel if (even and i!=0) else "pause"
-        windowsMap['window'].append(window)
-        windowsMap['label'].append(label)
-    df = pd.DataFrame(windowsMap)
-    df.columns = ['window', 'label']
-    return df
+def channels_to_list_df(channels_data, artifact_label):
+    # shape --> (channels, windows, samples)
+
+    channels_list_df = []
+
+    for channelData in channels_data:
+        channel_data_map = {'label': []}
+
+        for iWindow, window in enumerate(channelData):
+            even = (iWindow % 2) == 0
+            label = artifact_label if (even and iWindow != 0) else "pause"
+            channel_data_map['label'].append(label)
+
+            for iSample, sample in enumerate(window):
+                if iSample in channel_data_map:
+                    channel_data_map[iSample].append(sample)
+                else:
+                    channel_data_map[iSample] = [sample]
+
+        channel_data_df = pd.DataFrame(channel_data_map)
+        channels_list_df.append(channel_data_df)
+        # df.columns = ['window', 'label']
+
+    # array --> df(window, sample) + last column label
+    return channels_list_df
